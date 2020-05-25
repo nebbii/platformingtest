28a18f286ed7733d18a84ea82f0a4d2875fedd05
Ben W <ben.wolthuis@gmail.com>
2020-05-25 20:10:39 +0200

    Level renders, Testlevel properly included with tilesets

---
+++ core/assets/level/testleveltileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test2tiled" tilewidth="50" tileheight="50" tilecount="492" columns="41">
+ <image source="test.png" width="2068" height="641"/>
+</tileset>
--- core/src/com/nebsting/platformingtest/Core.java
+++ core/src/com/nebsting/platformingtest/Core.java
@@ -6,17 +6,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.MapRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class Core extends Game {
     OrthographicCamera camera;
 	SpriteBatch batch;
 
     TiledMap map;
-    TiledMapTileLayer[] maplayers;
+    TiledMapTileLayer[] layer;
 
+    MapRenderer mapRenderer;
     Player player;
 	
 	@Override
@@ -26,7 +29,14 @@ public class Core extends Game {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
 
-        map = new TmxMapLoader().load("testlevel.tmx");
+        map = new TmxMapLoader().load("level/testlevel.tmx");
+
+        layer = new TiledMapTileLayer[2];
+        //layer[0] = (TiledMapTileLayer)map.getLayers().get("visual");
+        layer[1] = (TiledMapTileLayer)map.getLayers().get("collide");
+
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
         player = new Player();
 	}
 
@@ -36,6 +46,9 @@ public class Core extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         player.logic();
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
 
         batch.setProjectionMatrix(camera.combined);
 		batch.begin();
